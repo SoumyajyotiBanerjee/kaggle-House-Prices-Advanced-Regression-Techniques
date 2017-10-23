28415c4b00aed99cb07b79eb2100ac075285469c
SoumyajyotiBanerjee <banerjee.soumyakyoti4@gmail.com>
2017-10-23 21:40:58 +0530

    removed NaN for continues variables

--- preprocessing.py
+++ preprocessing.py
@@ -7,6 +7,8 @@ This is a temporary script file.
 import pandas as pd
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+
 
 os.chdir("E:\kaggle")
 
@@ -51,22 +53,30 @@ list_null = df2.isnull().any()
 need_care = []
 c=0
 #Need special care -1 | Replace by Mean as continues - 0 | need no care - 2
+p=[]
 for e in list_null:
     if (e and type_col[c]==1):
         need_care.append((ls[c],1))
     if e and type_col[c]==0:
         need_care.append((ls[c],0))
+        p.append(ls[c])
     if not e:
         need_care.append((ls[c],2))
     #print c,type_col[c],ls[c], e
     c+=1
    
 Y = df2['SalePrice']
+
+print p
+
+print  sum([True for idx,row in df2.iterrows() if any(row.isnull())])
+print df2.shape
 #print need_care
 
+'''
 df3=df2[df2.BsmtQual.isnull()]
 dfs_dictionary = {'DF1':df2,'DF2':df3}
-df5 = df4.drop_duplicates(keep=False)
+df5 = dfs_dictionary.drop_duplicates(keep=False)
 
 df5.shape
 
@@ -98,7 +108,30 @@ print temp
 
 #print temp_dummies_LotShape
 
+'''
+#df2.plot( y=["LotFrontage", "MasVnrArea", "GarageYrBlt"], kind="line")
 
-    
+#plt.show()
 
-    
+df2['LotFrontage'].fillna(df2['LotFrontage'].median(), inplace=True)
+df2['MasVnrArea'].fillna(df2['MasVnrArea'].median(), inplace=True)
+df2['GarageYrBlt'].fillna(df2['GarageYrBlt'].median(), inplace=True)
+   
+
+print  sum([True for idx,row in df2.iterrows() if any(row.isnull())])
+print df2.shape
+
+p=[]
+c=0
+for e in list_null:
+    if (e and type_col[c]==1):
+        need_care.append((ls[c],1))
+    if e and type_col[c]==0:
+        need_care.append((ls[c],0))
+        p.append(ls[c])
+    if not e:
+        need_care.append((ls[c],2))
+    #print c,type_col[c],ls[c], e
+    c+=1
+
+print p
